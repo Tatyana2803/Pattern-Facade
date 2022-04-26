b5e4195e10410aa0ea9e35a714d80a49229858a2
Кондакова Татьяна <rotaru2803@rambler.ru>
2022-04-26 13:58:58 +0500

    add java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.mult("11101010", "001011101"));
+        System.out.println(bins.sum("11101010", "001011101"));
+    }
+}
